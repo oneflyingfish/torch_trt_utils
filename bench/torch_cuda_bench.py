aefcc25fdc2c38c5bac0a1765665d462ca470c23
yutian6 <yutian6@mgtv.com>
2025-08-07 16:56:06 +0800

    add cuda bench

--- bench/torch_cuda_bench.py
+++ bench/torch_cuda_bench.py
@@ -1,22 +1,9 @@
 from .time_bench import TIME_PERF
-import torch
+from .cuda_bench import test_time_cuda
+
 
 def test_torch_cuda_time(enable=TIME_PERF):
-    def wrapper0(func):
-        def wrapper(*args, **kwargs):
-            if enable:
-                start = torch.cuda.Event(enable_timing=True)
-                end = torch.cuda.Event(enable_timing=True)
-                torch.cuda.synchronize()
-                start.record()
-                output = func(*args, **kwargs)
-                end.record()
-                torch.cuda.synchronize()
-                print(f"<@{func.__qualname__}>: {start.elapsed_time(end)*0.001:.3f}s")
-                return output
-            else:
-                return func(*args, **kwargs)
-
-        return wrapper
-
-    return wrapper0
+    print(
+        "warning: you have use deperated function, use bench.cuda_bench.test_time_cuda instead"
+    )
+    return test_time_cuda(enable=enable)
